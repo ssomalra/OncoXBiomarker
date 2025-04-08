89bc654fe4203f643b3088096265fa3577b202fd
Sahiti Somalraju <116883221+ssomalra@users.noreply.github.com>
2025-04-08 15:56:45 -0400

    Update bar_chart.js

--- cancerdb/static/js/bar_chart.js
+++ cancerdb/static/js/bar_chart.js
@@ -9,7 +9,6 @@ document.addEventListener("DOMContentLoaded", function () {
         datasets: [{
           data: [11, 8, 6, 9, 35, 28], // You can replace these with dynamic values if needed
           backgroundColor: [
-            //'#e6c46a', '#3f78cc', '#fc90d1', '#5ad6b9', '#9ed5f0', '#74787a'
             '#f7c32e', '#6c88f0', '#f7a6d6', '#00bfae', '#a4c4f3', '#8f8f8f'
           ],
           borderWidth: 1
